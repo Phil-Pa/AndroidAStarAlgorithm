a50ce76a11014e32fc8a5e594c0396b3ddf085ce
Phil-Pa <philipparold.pa@gmail.com>
2020-08-05 18:12:38 +0200

    added basic background drawing

--- app/src/main/java/de/phil/astaralgorithm/AStarAlgorithmView.kt
+++ app/src/main/java/de/phil/astaralgorithm/AStarAlgorithmView.kt
@@ -1,7 +1,9 @@
 package de.phil.astaralgorithm
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
@@ -15,23 +17,47 @@ class AStarAlgorithmView(context: Context, attrs: AttributeSet) : View(context, 
     private var pathTileColor = Color.YELLOW
     private var emptyTileColor = Color.WHITE
 
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     init {
+        initCustomViewAttributes(context, attrs)
+        paint.style = Paint.Style.FILL
+        paint.color = emptyTileColor
+    }
+
+    private fun initCustomViewAttributes(
+        context: Context,
+        attrs: AttributeSet
+    ) {
         context.theme.obtainStyledAttributes(
             attrs,
             R.styleable.AStarAlgorithmView,
-            0, 0).apply {
+            0, 0
+        ).apply {
 
             try {
                 numTiles = getInteger(R.styleable.AStarAlgorithmView_numTiles, 10)
-                showDebugInformation = getBoolean(R.styleable.AStarAlgorithmView_showDebugInformation, false)
-                obstacleTileColor = getColor(R.styleable.AStarAlgorithmView_obstacleTileColor, Color.BLACK)
+                showDebugInformation =
+                    getBoolean(R.styleable.AStarAlgorithmView_showDebugInformation, false)
+                obstacleTileColor =
+                    getColor(R.styleable.AStarAlgorithmView_obstacleTileColor, Color.BLACK)
                 pathTileColor = getColor(R.styleable.AStarAlgorithmView_pathTileColor, Color.YELLOW)
-                emptyTileColor = getColor(R.styleable.AStarAlgorithmView_emptyTileColor, Color.WHITE)
+                emptyTileColor =
+                    getColor(R.styleable.AStarAlgorithmView_emptyTileColor, Color.WHITE)
             } finally {
                 recycle()
             }
         }
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+
+        if (canvas == null)
+            return
+
+        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paint)
+
+        super.onDraw(canvas)
     }
 
 
